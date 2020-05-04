e01d103874e9c5ff500594b8aa4e9b02199ea284
Daniel Rudich <rudich47@gmail.com>
2020-05-04 13:48:08 -0400

    fixed page

--- jobs/views.py
+++ jobs/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 from .models import Job
 
 
-def home(request):
+def index(request):
     jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    return render(request, 'jobs/index.html', {'jobs':jobs})
--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -9,8 +9,8 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name = 'home'),
-    
+    path('', jobs.views.index, name = 'index'),
+
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
